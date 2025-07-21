67fb0d0b54bef540f5c4c779e2143c6e4cf73f6d
Evan Harley <evan.harley21@gmail.com>
2025-07-21 11:36:47 -0700

    refactor: migrate from cloudflare-workers to cloudflare pages adapter for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-cloudflare-workers';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,16 +7,17 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter({
-		config: 'wrangler.jsonc',
-		fallback: 'plaintext',
-		routes: {
-			include: ['/*'],
-			exclude: ['<all>']
-		}
-	}) , alias: {
-		"@/*": "./path/to/lib/*",
-	  }
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			config: 'wrangler.jsonc',
+			fallback: 'plaintext',
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		}),
+		alias: { '@/*': './path/to/lib/*' }
 	},
 	extensions: ['.svelte', '.svx']
 };
