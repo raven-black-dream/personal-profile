7315403d028a6f1501e2b6036deeff8d77a830e5
Evan Harley <evan.harley21@gmail.com>
2025-07-21 11:25:37 -0700

    feat: add Cloudflare Workers configuration and cleanup Svelte config formatting

--- svelte.config.js
+++ svelte.config.js
@@ -7,9 +7,9 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() , alias: {
-		"@/*": "./path/to/lib/*",
-	  }
+	kit: {
+		adapter: adapter(),
+		alias: { '@/*': './path/to/lib/*' }
 	},
 	extensions: ['.svelte', '.svx']
 };
